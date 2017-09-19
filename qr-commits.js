0b1ebbd11c85a7635ee70d5ea1c832df19731fef
matt.fehskens <matt.fehskens@gmail.com>
2017-09-19 10:49:53 -0400

    Promisified query, added single GET & repo GET

--- qr-commits.js
+++ qr-commits.js
@@ -1,5 +1,6 @@
 'use latest';
 
+import { promisifyAll } from 'bluebird';
 import * as mysql from 'mysql';
 
 module.exports = (context, request, response) => {
@@ -14,17 +15,33 @@ module.exports = (context, request, response) => {
 const handleGet = (context, response) => {
     const dbSetup = getDbSetup(context.data);
     const dbConnection = mysql.createConnection(dbSetup);
-    const sql = 'SELECT * FROM commits ORDER BY pushed_at DESC';
+    const db = promisifyAll(dbConnection);
+    let promise;
 
-    dbConnection.query(sql, (error, commits) => {
-        if (error) {
-            sendJson(response, 400, { error });
-        } else {
-            sendJson(response, 200, { commits });
-        }
-        dbConnection.end();
-    });
+    if (context.data.commit) {
+        promise = getSingleCommit(db, context.data.commit);
+    } else if (context.data.repo) {
+        promise = getRepoCommits(db, context.data.repo);
+    } else {
+        promise = getAllCommits(db);
+    }
+
+    promise.then((data) => sendJson(response, 200, data))
+        .catch((error) => sendJson(response, 400, { error }))
+        .finally(() => dbConnection.end());
 };
+
+const getSingleCommit = (db, commit) =>
+    db.queryAsync('SELECT * FROM commits WHERE commit_url = ?', [commit])
+        .then((commit) => ({ commit }));
+
+const getRepoCommits = (db, repoUrl) =>
+    db.queryAsync('SELECT * FROM commits WHERE repo_url = ?', [repoUrl])
+        .then((commits) => ({ commits }));
+
+const getAllCommits = (db) =>
+    db.queryAsync('SELECT * FROM commits ORDER BY pushed_at DESC')
+        .then((commits) => ({ commits }));
 
 const getDbSetup = (data) => {
   const {
