a55578eccf51dd9e9f19abbacdb46399fdd3c52b
matt.fehskens <matt.fehskens@gmail.com>
2017-09-19 12:26:29 -0400

    Forgot a curly brace

--- qr-hook.js
+++ qr-hook.js
@@ -12,7 +12,7 @@ module.exports = (context, callback) => {
   if (data.head_commit && data.head_commit.url) {
     qrDataUrl(data.head_commit.url)
       .then((url) => commitToDb(url, data))
-      .then((message) => callback(null, message }))
+      .then((message) => callback(null, { message }))
       .catch((error) => callback(null, { error }));
   } else if (data.hook) {
     callback(null, { pong: `Looks like you want me to say "pong"` });
